acc994475067c00b56c529735fcd1b4cc41355e4
srikar-pasumarthy <srikarp@email.unc.edu>
2023-08-01 19:18:18 -0400

    Put Coming Soon Text.

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,15 +1,27 @@
-import { OrbitControls } from '@react-three/drei'
+import { Center, Float, OrbitControls, Text3D } from '@react-three/drei'
 
 export default function Experience()
 {
     return <>
 
         <OrbitControls makeDefault />
-        
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
 
+        {/* <Text3D>Hello</Text3D> */}
+        <Float>
+            <Center>
+                <Text3D 
+                font="./fonts/helvetiker_regular.typeface.json" 
+                bevelEnabled
+                bevelSize={0.04}
+                bevelThickness={0.1}
+                height={0.1}
+                lineHeight={0.5}
+                letterSpacing={-0.0001}
+                size={0.3}>
+                    {`Coming         Soon`}
+                    <meshNormalMaterial />
+                </Text3D>
+            </Center>
+        </Float>
     </>
 }
